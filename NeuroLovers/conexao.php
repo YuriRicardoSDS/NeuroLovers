9b8c2b4c75a06e207e10675efb0649c74a594c5f
YuriRicardoSDS <166342523+YuriRicardoSDS@users.noreply.github.com>
2025-08-04 13:09:54 -0300

    Update conexao.php

--- NeuroLovers/conexao.php
+++ NeuroLovers/conexao.php
@@ -1,12 +1,16 @@
 <?php
+// Credenciais do banco de dados
 $servidor = "localhost";
 $usuario = "root";
 $senha = "";
 $banco = "neurolovers";
 
-$conexao = mysqli_connect($servidor, $usuario, $senha, $banco);
+// Cria a conexão com o banco de dados
+// A variável foi renomeada para $conn para ser compatível com homepage.php
+$conn = mysqli_connect($servidor, $usuario, $senha, $banco);
 
-if(!$conexao){
+// Verifica se a conexão falhou
+if(!$conn){
     die("Falha na conexao: ".mysqli_connect_error());
 }
 ?>
